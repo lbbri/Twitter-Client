87e2991b6de03ea2379b3b7429fff29ed95a5d16
Briana Mason <brm14@fb.com>
2020-06-30 19:08:35 -0700

    User can compose and post a Tweet.

---
+++ twitter/View Controllers/ComposeViewController.h
@@ -0,0 +1,19 @@
+//
+//  ComposeViewController.h
+//  twitter
+//
+//  Created by brm14 on 6/30/20.
+//  Copyright © 2020 Emerson Malca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ComposeViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UITextView *composedTweetView;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ twitter/View Controllers/ComposeViewController.m
@@ -0,0 +1,50 @@
+//
+//  ComposeViewController.m
+//  twitter
+//
+//  Created by brm14 on 6/30/20.
+//  Copyright © 2020 Emerson Malca. All rights reserved.
+//
+
+#import "ComposeViewController.h"
+#import "APIManager.h"
+
+@interface ComposeViewController ()
+
+@end
+
+@implementation ComposeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+- (IBAction)cancelClick:(id)sender {
+    [self dismissViewControllerAnimated:true completion:nil];
+}
+
+
+- (IBAction)tweetClick:(id)sender {
+    
+    NSString *newTweet = self.composedTweetView.text;
+    [[APIManager shared] postStatusWithText:newTweet completion:^(Tweet *posting, NSError *error) {
+        NSLog(@"Success");
+    }];
+    
+    [self dismissViewControllerAnimated:true completion:nil];
+
+}
+
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
